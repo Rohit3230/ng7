086d93f43b517d0d33963a87174c908b3a9eac55
RSN <rsn3230@gmail.com>
2020-02-23 14:14:30 +0530

    commit

---
+++ userApp/src/app/home-page-component/home-page-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-component',
+  templateUrl: './home-page-component.component.html',
+  styleUrls: ['./home-page-component.component.css']
+})
+export class HomePageComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
